48a7817c7526306e07b2d809b5918f3c52cc04c9
Emilo <emilio.furnari87@gmail.com>
2020-12-16 16:00:56 +0100

    finito

--- js/myscript.js
+++ js/myscript.js
@@ -5,17 +5,15 @@ var app = new Vue({
     mail: "",
     mailList: [],
   },
-  methods:{
+  created: function (){
+    // FACCIO 10 CHIAMATE AL SERVER
+    for (var i = 0; i < 10; i++) {
+      axios.get('https://flynn.boolean.careers/exercises/api/random/mail') .then((response) => {
+        // SALVO IL RISULTATO DELLA CHIAMATA IN UN DATA
+        this.mail = response.data.response;
+        this.mailList.push(this.mail);
+      });
+    }
+   }
 
-
-  }
 })
-
-// FACCIO LA CHIAMATA AL SERVER PER 10 VOLTE
-for (var i = 0; i < 10; i++) {
-  axios.get('https://flynn.boolean.careers/exercises/api/random/mail') .then(function (response) {
-    // SALVO IL RISULTATO DELLA CHIAMATA IN UN DATA
-    this.mail = response.data;
-    console.log(this.mail);
-  });
-}
