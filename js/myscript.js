fe4678a051f87e3908e35bd9dd1032be1546b60b
Emilo <emilio.furnari87@gmail.com>
2020-12-16 14:45:49 +0100

    caricate CDN Vue e Ajax

--- js/myscript.js
+++ js/myscript.js
@@ -1,1 +1,10 @@
 // Esercizio: Generare 10 email e stamparle a schermo, utilizzando Vue, Axios e l'endpoint relativo nelle slide che sto per aggiungere su Drive.
+var app = new Vue({
+  el: '#app',
+  data: {
+
+  },
+  methods:{
+    
+  }
+})
