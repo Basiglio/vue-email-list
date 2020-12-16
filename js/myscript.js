eb4ab88bce0e0ddbd2832273656b940039b373b8
Emilo <emilio.furnari87@gmail.com>
2020-12-16 16:22:24 +0100

    gestite le chiamate tramite bottone

--- js/myscript.js
+++ js/myscript.js
@@ -4,16 +4,29 @@ var app = new Vue({
   data: {
     mail: "",
     mailList: [],
+    mailList2: [],
+    message_1: "Qui 10 email casuali",
+    message_2: "qui premi per generarne altre 10"
   },
   created: function (){
     // FACCIO 10 CHIAMATE AL SERVER
     for (var i = 0; i < 10; i++) {
-      axios.get('https://flynn.boolean.careers/exercises/api/random/mail') .then((response) => {
+      axios.get('https://flynn.boolean.careers/exercises/api/random/mail') .then((result) => {
+        console.log(result);
         // SALVO IL RISULTATO DELLA CHIAMATA IN UN DATA
-        this.mail = response.data.response;
+        this.mail = result.data.response;
         this.mailList.push(this.mail);
       });
     }
-   }
-
+  },
+  methods:{
+    callMail: function (){
+      for (var i = 0; i < 10; i++) {
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail') .then((result) => {
+           this.mail = result.data.response;
+           this.mailList2.push(this.mail);
+        })
+      }
+    }
+  }
 })
